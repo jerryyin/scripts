a3368a7bb34bb8de5481e1b798944edbd3689d9b
jerryyin <zhuoryin@amd.com>
2020-10-31 22:20:22 -0500

    Install python3-dev

--- docker/init.sh
+++ docker/init.sh
@@ -4,12 +4,12 @@ set -x
 shopt -s expand_aliases
 alias dockerInstall='DEBIAN_FRONTEND=noninteractive apt-get install -f -y'
 
-apt-get update --allow-insecure-repositories 
+apt-get update --allow-insecure-repositories
 
 dockerInstall software-properties-common  # Install add-apt-repository
 dockerInstall apt-transport-https         # Dependency from kitware, for https
 dockerInstall wget
-                   
+
 # PPA:  TODO remove when it becomes default ubuntu package
 # vim8 packge ppa.
 add-apt-repository -y ppa:jonathonf/vim
@@ -22,8 +22,8 @@ wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | 
 apt-add-repository 'deb https://apt.kitware.com/ubuntu/ xenial main'
 
 # Install misc pkgs
-dockerInstall apt-utils ssh curl universal-ctags global cscope git
-dockerInstall vim stow xclip locales python-autopep8 gdb tmux zsh
+dockerInstall apt-utils ssh curl universal-ctags global cscope git zsh
+dockerInstall vim stow xclip locales python3-dev python3-autopep8 gdb tmux
 # https://github.com/google/llvm-premerge-checks/blob/master/containers/base-debian/Dockerfile
 dockerInstall clang-10 lld-10 clang-tidy-10 clang-format-10
 dockerInstall cmake       # MLIR package
