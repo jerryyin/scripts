6bb7212ed4f09653179a991b104a6f2d3007fd4a
jerryyin <zhuoryin@amd.com>
2022-03-19 15:41:45 -0500

    Fix broken dependencies

--- docker/init.sh
+++ docker/init.sh
@@ -2,9 +2,9 @@
 set -x
 
 shopt -s expand_aliases
-alias dockerInstall='DEBIAN_FRONTEND=noninteractive apt-get install -f -y'
+alias dockerInstall='DEBIAN_FRONTEND=noninteractive sudo apt-get install -f -y'
 
-apt-get update --allow-insecure-repositories
+sudo apt-get update --allow-insecure-repositories
 
 dockerInstall software-properties-common  # Install add-apt-repository
 dockerInstall apt-transport-https         # Dependency from kitware, for https
@@ -12,24 +12,22 @@ dockerInstall wget
 
 # PPA:  TODO remove when it becomes default ubuntu package
 # vim8 packge ppa.
-add-apt-repository -y ppa:jonathonf/vim
-# universal ctags ppa.
-add-apt-repository -y ppa:hnakamur/universal-ctags
+sudo add-apt-repository -y ppa:jonathonf/vim
 # gnu global ppa.
-add-apt-repository -y ppa:dns/gnu
+sudo add-apt-repository -y ppa:dns/gnu
 # cmake, dependent on apt-transport-https. Refer to https://apt.kitware.com
-wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | apt-key add -
-apt-add-repository 'deb https://apt.kitware.com/ubuntu/ xenial main'
+wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | sudo apt-key add -
+sudo apt-add-repository 'deb https://apt.kitware.com/ubuntu/ xenial main'
 
 # Install misc pkgs
-dockerInstall apt-utils ssh curl universal-ctags global cscope git
-dockerInstall vim stow xclip locales python3-dev python-autopep8 gdb
+dockerInstall apt-utils ssh curl global cscope git
+dockerInstall vim stow xclip locales python3-dev python3-autopep8 gdb
 # For macos: the_silver_searcher
 dockerInstall zsh fonts-powerline tmux silversearcher-ag
 # https://github.com/google/llvm-premerge-checks/blob/master/containers/base-debian/Dockerfile
 dockerInstall clang-10 lld-10 clang-tidy-10 clang-format-10 cmake ninja-build 
 
-apt-get clean && rm -rf /var/lib/apt/lists/*
+sudo apt-get clean && rm -rf /var/lib/apt/lists/*
 
 # install tmux plugin manager
 if [ ! -d .tmux/plugins/tpm ]; then
@@ -45,7 +43,7 @@ if [ ! -d .oh-my-zsh ]; then
 fi
 
 # Make zsh default shell
-chsh -s $(which zsh)
+sudo chsh -s $(which zsh)
 
 # rc files
 if [ ! -d rc_files ]; then
