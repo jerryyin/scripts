f9bac0cc0f6b8355896bc701fb26a1a0f9ab18b2
Zhuoran Yin <zhuoryin@amd.com>
2022-07-16 17:08:36 -0500

    Update init_priv.sh

--- docker/init_priv.sh
+++ docker/init_priv.sh
@@ -8,8 +8,3 @@ if [ ! -f ~/.ssh/id_rsa ]; then
     cp -r /zyin/.ssh ~/.ssh
     chmod 400 ~/.ssh/id_rsa && ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
 fi
-
-if [ ! -d Documents/notes ]; then
-    mkdir -p Documents/notes
-    git clone git@github.com:jerryyin/notes.git Documents/notes
-fi
