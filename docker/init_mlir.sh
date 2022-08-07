c10d3a1cb188aa7e2bda94b8e1072864cddc41d0
jerryyin <zhuoryin@amd.com>
2022-08-07 15:12:42 +0000

    Refactor mlir setup script

--- docker/init_mlir.sh
+++ docker/init_mlir.sh
@@ -3,12 +3,18 @@ set -x
 
 #bash $(dirname "$0")/init.sh
 
+setupMlirTag () {
+  find "$1"/mlir -type f -print > "$1"/gtags.files
+
+  pushd "$1"
+  gtags
+  popd
+}
+
 if [ ! -d llvm-project-mlir ]; then
   git clone https://github.com/ROCmSoftwarePlatform/llvm-project-mlir.git
-  cd llvm-project-mlir
-  find $(pwd)/mlir -type f -print > gtags.files && gtags
+  setupMlirTag "${PWD}/llvm-project-mlir"
   git -C llvm-project-mlir remote set-url origin git@github.com:ROCmSoftwarePlatform/llvm-project-mlir.git
-  cd ~
+else
+  setupMlirTag "${PWD}/llvm-project-mlir"
 fi
-
-
