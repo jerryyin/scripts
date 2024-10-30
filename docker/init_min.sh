86f9890eb582a8a7836799bb45c6a27fc2e40c4b
jerryyin <zhuoryin@amd.com>
2024-10-30 14:27:06 +0000

    Adding git

--- docker/init_min.sh
+++ docker/init_min.sh
@@ -24,7 +24,7 @@ add-apt-repository -y ppa:jonathonf/vim
 add-apt-repository -y ppa:neovim-ppa/stable
 
 # Install misc pkgs (For macos: the_silver_searcher)
-dockerInstall vim zsh fonts-powerline tmux silversearcher-ag less stow nodejs neovim
+dockerInstall git vim zsh fonts-powerline tmux silversearcher-ag less stow nodejs neovim
 sudo apt-get clean && rm -rf /var/lib/apt/lists/*
 
 # install tmux plugin manager
