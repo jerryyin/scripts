cca2797a7a087522de84fabd2d54bb1e13c34971
jerryyin <zhuoryin@amd.com>
2024-12-12 16:58:02 +0000

    Moving hostname fix to container launch time

--- docker/init_priv.sh
+++ docker/init_priv.sh
@@ -8,3 +8,8 @@ if [ ! -f ~/.ssh/id_rsa ]; then
     cp -r /zyin/.ssh ~/.ssh
     chmod 400 ~/.ssh/id_rsa && ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
 fi
+
+# Fixing /etc/host file, refer to https://askubuntu.com/questions/59458/error-message-sudo-unable-to-resolve-host-none
+if ! grep -q "$HOSTNAME" /etc/hosts; then
+    echo $(hostname -I | cut -d\  -f1) $(hostname) | sudo -h 127.0.0.1 tee -a /etc/hosts
+fi
