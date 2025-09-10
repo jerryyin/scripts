8d599c82a5895248eeebb95346532840d3ba0190
jerryyin <zhuoryin@amd.com>
2025-09-10 14:21:59 +0000

    Adding therock for rocprofilerv3

--- iree/setup_att.sh
+++ iree/setup_att.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-
-mkdir -p ~/att && cd ~/att
+# Note that aqlprofile is still needed for rocprofv3 in tarball
 dpkg -i /zyin/hsa-amd-aqlprofile_1.0.0-local_amd64_rocm6.3.deb
 pip install websockets matplotlib
+
+# Download therock and find ~/install/bin/roprofv3 for latest rocprof
+# This can be dropped after att support becomes default
+wget https://github.com/ROCm/TheRock/releases/download/nightly-tarball/therock-dist-linux-gfx94X-dcgpu-7.0.0rc20250701.tar.gz
+mkdir install
+tar -xf ./therock-dist-linux-gfx94X-dcgpu-7.0.0rc20250701.tar.gz -C install
