7bdd3e7b09e0b8013ac5f3e20bbf9503764f4788
jerryyin <zhuoryin@amd.com>
2025-09-17 20:50:37 +0000

    Install rocprof trace decoder lib

--- iree/setup_att.sh
+++ iree/setup_att.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Note that aqlprofile is still needed for rocprofv3 in tarball
-dpkg -i /zyin/hsa-amd-aqlprofile_1.0.0-local_amd64_rocm6.3.deb
+# dpkg -i /zyin/hsa-amd-aqlprofile_1.0.0-local_amd64_rocm6.3.deb
 pip install websockets matplotlib
 
 # Download therock and find ~/install/bin/roprofv3 for latest rocprof
@@ -9,3 +9,7 @@ pip install websockets matplotlib
 wget https://github.com/ROCm/TheRock/releases/download/nightly-tarball/therock-dist-linux-gfx94X-dcgpu-7.0.0rc20250701.tar.gz
 mkdir install
 tar -xf ./therock-dist-linux-gfx94X-dcgpu-7.0.0rc20250701.tar.gz -C install
+
+wget https://github.com/ROCm/rocprof-trace-decoder/releases/download/0.1.4/rocprof-trace-decoder-manylinux-2.28-0.1.4-Linux.sh 
+bash ./rocprof-trace-decoder-manylinux-2.28-0.1.4-Linux.sh --skip-license --prefix="/"
+ls /opt/rocm/lib/librocprof-trace-decoder.so
