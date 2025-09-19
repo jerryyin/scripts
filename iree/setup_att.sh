5a9bf4c4cf468b1d568b50e303c9741cf3f9a6f2
jerryyin <zhuoryin@amd.com>
2025-09-19 20:39:44 +0000

    Stop using theRock since default rocprofv3 is okay

--- iree/setup_att.sh
+++ iree/setup_att.sh
@@ -6,9 +6,9 @@ pip install websockets matplotlib
 
 # Download therock and find ~/install/bin/roprofv3 for latest rocprof
 # This can be dropped after att support becomes default
-wget https://github.com/ROCm/TheRock/releases/download/nightly-tarball/therock-dist-linux-gfx94X-dcgpu-7.0.0rc20250701.tar.gz
-mkdir install
-tar -xf ./therock-dist-linux-gfx94X-dcgpu-7.0.0rc20250701.tar.gz -C install
+#wget https://github.com/ROCm/TheRock/releases/download/nightly-tarball/therock-dist-linux-gfx94X-dcgpu-7.0.0rc20250701.tar.gz
+#mkdir install
+#tar -xf ./therock-dist-linux-gfx94X-dcgpu-7.0.0rc20250701.tar.gz -C install
 
 wget https://github.com/ROCm/rocprof-trace-decoder/releases/download/0.1.4/rocprof-trace-decoder-manylinux-2.28-0.1.4-Linux.sh 
 bash ./rocprof-trace-decoder-manylinux-2.28-0.1.4-Linux.sh --skip-license --prefix="/"
