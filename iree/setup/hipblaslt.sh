bcc217694d980c761426d4a710169c153ba87607
jerryyin <zhuoryin@amd.com>
2025-10-28 18:33:58 +0000

    Adjustment to hipblaslt build

--- iree/setup/hipblaslt.sh
+++ iree/setup/hipblaslt.sh
@@ -3,7 +3,7 @@ set -e
 
 pip install joblib
 sudo apt update
-sudo apt install liblapack-dev libblas-dev gfortran libgtest-dev libboost-filesystem-dev
+sudo apt install hipblas-common-dev liblapack-dev libblas-dev gfortran libgtest-dev libboost-filesystem-dev libmsgpack-cxx-dev libgtest-dev google-mock
 
 cd ~
 git clone --no-checkout --filter=blob:none https://github.com/ROCm/rocm-libraries.git
@@ -13,6 +13,8 @@ git sparse-checkout set projects/hipblaslt shared
 git checkout develop # or the branch you are starting from
 
 cd ~/rocm-libraries/projects/hipblaslt
+# Newer cmake is incompatible with below script
+bash ./install.sh -idc || true
 mkdir -p build
 cmake -B build -S .                                  \
       -D CMAKE_BUILD_TYPE=Release                    \
