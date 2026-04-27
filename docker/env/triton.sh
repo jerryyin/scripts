082412fa358fcf50c811548766abda74afb0992e
jerryyin <zhuoryin@amd.com>
2026-04-27 19:10:10 +0000

    Install clang-d in triton env

--- docker/env/triton.sh
+++ docker/env/triton.sh
@@ -10,7 +10,7 @@ set -x
 wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | sudo tee /usr/share/keyrings/kitware-archive-keyring.gpg >/dev/null
 echo 'deb [signed-by=/usr/share/keyrings/kitware-archive-keyring.gpg] https://apt.kitware.com/ubuntu/ noble main' | sudo tee /etc/apt/sources.list.d/kitware.list >/dev/null
 sudo apt-get update --allow-insecure-repositories -qq && sudo apt-get install -f -y -qq kitware-archive-keyring
-sudo apt-get update && sudo apt-get install -f -y cmake ccache ninja-build
+sudo apt-get update && sudo apt-get install -f -y cmake ccache ninja-build clangd
 
 python -m pip config set global.break-system-packages true
 
